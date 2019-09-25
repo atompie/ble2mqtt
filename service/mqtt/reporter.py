3dcd6a36e1919eb25aecfcb82ffc32c66af90dc4
risto <risto.kowaczewski@gmail.co>
2019-09-25 20:43:09 +0200

    CMND topic.

--- service/mqtt/reporter.py
+++ service/mqtt/reporter.py
@@ -19,9 +19,10 @@ class MqttReporter:
         self.require_update = False
 
     def _on_cmnd(self, client, userdata, msg):
-        print(msg.payload)
+        print(msg.payload, str(msg.payload).lower() == b'update', msg.payload == b'update', msg.payload.lower() == b'update')
         if str(msg.payload).lower() == b'update':
             self.require_update = True
+            print("{}/CMND {}".format(self._topic, msg.payload))
             self._logger.info("{}/CMND {}".format(self._topic, msg.payload))
 
     def enters(self, ble: dict):
